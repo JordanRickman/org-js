2a32ae9387c4257ae5d1f2e89214f9b811fbc547
Jordan Rickman <jordan.rickman.42@gmail.com>
2016-09-20 22:38:29 -0700

    More token-matching regexps and their unit tests. Not all regexps have been tested.

--- test/lexer.js
+++ test/lexer.js
@@ -176,47 +176,6 @@ describe('Token-matching Regular Expressions', function() {
      "Failed on '"+todoItem+"', got:\n["+execResult+"]");
   });
 
-  it('should properly parse preformatted blocks', function() {
-    var preformatted = tokenRegExps['preformatted'];
-    var execResult;
-
-    var indented = "      :   Only the first space after ':' is lost. #%547205#$^*(%$^%&*)#$@";
-    execResult = preformatted.exec(indented);
-    assert(regExpResultEquals(execResult, [indented, "      ", "  Only the first space after ':' is lost. #%547205#$^*(%$^%&*)#$@"]),
-      "Failed on '"+indented+"', got:\n["+execResult+"]");
-
-    var notIndented = ":   Only the first space after ':' is lost. #%547205#$^*(%$^%&*)#$@";
-    execResult = preformatted.exec(notIndented);
-    assert(regExpResultEquals(execResult, [notIndented, "", "  Only the first space after ':' is lost. #%547205#$^*(%$^%&*)#$@"]),
-      "Failed on '"+notIndented+"', got:\n["+execResult+"]");
-
-    var endsAfterColon = "      :";
-    execResult = preformatted.exec(endsAfterColon);
-    assert(regExpResultEquals(execResult, [endsAfterColon, "      ", ""]),
-      "Failed on '"+endsAfterColon+"', got:\n["+execResult+"]");
-
-    var emptyContents = "      : ";
-    execResult = preformatted.exec(emptyContents);
-    assert(regExpResultEquals(execResult, [emptyContents, "      ", ""]),
-      "Failed on '"+emptyContents+"', got:\n["+execResult+"]");
-
-    var minimal = ":";
-    execResult = preformatted.exec(minimal);
-    assert(regExpResultEquals(execResult, [minimal, "", ""]),
-      "Failed on '"+minimal+"', got:\n["+execResult+"]");
-
-    //----- Examples that shouldn't match ------
-    var noSpaceAfterColon = "      :Only the first space after ':' is lost. #%547205#$^*(%$^%&*)#$@";
-    execResult = preformatted.exec(noSpaceAfterColon);
-    assert(execResult === null,
-      "Failed on '"+noSpaceAfterColon+"', got:\n["+execResult+"]");
-
-    var noLeadingColon = "   Only the first space after ':' is lost. #%547205#$^*(%$^%&*)#$@";
-    execResult = preformatted.exec(noLeadingColon);
-    assert(execResult === null,
-      "Failed on '"+noLeadingColon+"', got:\n["+execResult+"]");
-  });
-
   it('should properly parse dictionary list elements', function() {
     var dictionaryListElem = tokenRegExps['dictionaryListElement'];
     var execResult;
@@ -402,5 +361,283 @@ describe('Token-matching Regular Expressions', function() {
     execResult = listElem.exec(noSpaceBeforeStar);
     assert(execResult === null,
       "Failed on '"+noSpaceBeforeStar+"', got:\n["+execResult+"]");
+  });
+
+  it('should properly parse drawer beginnings', function() {
+    var beginDrawer = tokenRegExps['beginDrawer'];
+    var execResult;
+
+    var minimal = "::";
+    execResult = beginDrawer.exec(minimal);
+    assert(regExpResultEquals(execResult, [minimal, "", ""]),
+      "Failed on '"+minimal+"', got:\n["+execResult+"]");
+
+    var normal = ":my_drawer-1:  ";
+    execResult = beginDrawer.exec(normal);
+    assert(regExpResultEquals(execResult, [normal, "", "my_drawer-1"]),
+      "Failed on '"+normal+"', got:\n["+execResult+"]");
+
+    var indented = "  \t :my_drawer-1:  ";
+    execResult = beginDrawer.exec(indented);
+    assert(regExpResultEquals(execResult, [indented, "  \t ", "my_drawer-1"]),
+      "Failed on '"+indented+"', got:\n["+execResult+"]");
+
+    //----- Examples that will not match -----
+    var noOpeningColon = "my_drawer-1:";
+    execResult = beginDrawer.exec(noOpeningColon);
+    assert(execResult === null,
+      "Failed on '"+noOpeningColon+"', got:\n["+execResult+"]");
+
+    var noClosingColon = ":my_drawer-1";
+    execResult = beginDrawer.exec(noClosingColon);
+    assert(execResult === null,
+      "Failed on '"+noClosingColon+"', got:\n["+execResult+"]");
+
+    var spaceAfterOpeningColon = ": my_drawer-1:";
+    execResult = beginDrawer.exec(spaceAfterOpeningColon);
+    assert(execResult === null,
+      "Failed on '"+spaceAfterOpeningColon+"', got:\n["+execResult+"]");
+
+    var illegalCharInName = ":my_$drawer-1:";
+    execResult = beginDrawer.exec(illegalCharInName);
+    assert(execResult === null,
+      "Failed on '"+illegalCharInName+"', got:\n["+execResult+"]");
+
+    var spaceAfterName = ":my_drawer-1 :";
+    execResult = beginDrawer.exec(spaceAfterName);
+    assert(execResult === null,
+      "Failed on '"+spaceAfterName+"', got:\n["+execResult+"]");
+
+    var somethingBeforeOpeningColon = "   a:my_drawer-1:";
+    execResult = beginDrawer.exec(somethingBeforeOpeningColon);
+    assert(execResult === null,
+      "Failed on '"+somethingBeforeOpeningColon+"', got:\n["+execResult+"]");
+
+    var somethingAfterClosingColon = ":my_drawer-1:  a ";
+    execResult = beginDrawer.exec(somethingAfterClosingColon);
+    assert(execResult === null,
+      "Failed on '"+somethingAfterClosingColon+"', got:\n["+execResult+"]");
+  });
+
+  it('should properly parse drawer endings', function() {
+    var endDrawer = tokenRegExps['endDrawer'];
+    var execResult;
+
+    var minimal = ":END:";
+    execResult = endDrawer.exec(minimal);
+    assert(regExpResultEquals(execResult, [minimal, ""]),
+      "Failed on '"+minimal+"', got:\n["+execResult+"]");
+
+    var indented = "  \t :END:  ";
+    execResult = endDrawer.exec(indented);
+    assert(regExpResultEquals(execResult, [indented, "  \t "]),
+      "Failed on '"+indented+"', got:\n["+execResult+"]");
+
+    //----- Examples that will not match -----
+    var noOpeningColon = "END:";
+    execResult = endDrawer.exec(noOpeningColon);
+    assert(execResult === null,
+      "Failed on '"+noOpeningColon+"', got:\n["+execResult+"]");
+
+    var noClosingColon = ":END";
+    execResult = endDrawer.exec(noClosingColon);
+    assert(execResult === null,
+      "Failed on '"+noClosingColon+"', got:\n["+execResult+"]");
+
+    var spaceAfterOpeningColon = ": END:";
+    execResult = endDrawer.exec(spaceAfterOpeningColon);
+    assert(execResult === null,
+      "Failed on '"+spaceAfterOpeningColon+"', got:\n["+execResult+"]");
+
+    var notEND = ":end:";
+    execResult = endDrawer.exec(notEND);
+    assert(execResult === null,
+      "Failed on '"+notEND+"', got:\n["+execResult+"]");
+
+    var spaceAfterName = ":END :";
+    execResult = endDrawer.exec(spaceAfterName);
+    assert(execResult === null,
+      "Failed on '"+spaceAfterName+"', got:\n["+execResult+"]");
+
+    var somethingBeforeOpeningColon = "   a:END:";
+    execResult = endDrawer.exec(somethingBeforeOpeningColon);
+    assert(execResult === null,
+      "Failed on '"+somethingBeforeOpeningColon+"', got:\n["+execResult+"]");
+
+    var somethingAfterClosingColon = ":END:  a ";
+    execResult = endDrawer.exec(somethingAfterClosingColon);
+    assert(execResult === null,
+      "Failed on '"+somethingAfterClosingColon+"', got:\n["+execResult+"]");
+  });
+
+  it('should properly parse preformatted blocks', function() {
+    var preformatted = tokenRegExps['preformatted'];
+    var execResult;
+
+    var indented = "      :   Only the first space after ':' is lost. #%547205#$^*(%$^%&*)#$@";
+    execResult = preformatted.exec(indented);
+    assert(regExpResultEquals(execResult, [indented, "      ", "  Only the first space after ':' is lost. #%547205#$^*(%$^%&*)#$@"]),
+      "Failed on '"+indented+"', got:\n["+execResult+"]");
+
+    var notIndented = ":   Only the first space after ':' is lost. #%547205#$^*(%$^%&*)#$@";
+    execResult = preformatted.exec(notIndented);
+    assert(regExpResultEquals(execResult, [notIndented, "", "  Only the first space after ':' is lost. #%547205#$^*(%$^%&*)#$@"]),
+      "Failed on '"+notIndented+"', got:\n["+execResult+"]");
+
+    var endsAfterColon = "      :";
+    execResult = preformatted.exec(endsAfterColon);
+    assert(regExpResultEquals(execResult, [endsAfterColon, "      ", ""]),
+      "Failed on '"+endsAfterColon+"', got:\n["+execResult+"]");
+
+    var emptyContents = "      : ";
+    execResult = preformatted.exec(emptyContents);
+    assert(regExpResultEquals(execResult, [emptyContents, "      ", ""]),
+      "Failed on '"+emptyContents+"', got:\n["+execResult+"]");
+
+    var tabDelimited = "\t\t:\tHere's some content";
+    execResult = preformatted.exec(tabDelimited);
+    assert(regExpResultEquals(execResult, [tabDelimited, "\t\t", "Here's some content"]),
+      "Failed on '"+tabDelimited+"', got:\n["+execResult+"]");
+
+    var minimal = ":";
+    execResult = preformatted.exec(minimal);
+    assert(regExpResultEquals(execResult, [minimal, "", ""]),
+      "Failed on '"+minimal+"', got:\n["+execResult+"]");
+
+    //----- Examples that shouldn't match ------
+    var noSpaceAfterColon = "      :Only the first space after ':' is lost. #%547205#$^*(%$^%&*)#$@";
+    execResult = preformatted.exec(noSpaceAfterColon);
+    assert(execResult === null,
+      "Failed on '"+noSpaceAfterColon+"', got:\n["+execResult+"]");
+
+    var noLeadingColon = "   Only the first space after ':' is lost. #%547205#$^*(%$^%&*)#$@";
+    execResult = preformatted.exec(noLeadingColon);
+    assert(execResult === null,
+      "Failed on '"+noLeadingColon+"', got:\n["+execResult+"]");
+  });
+
+  it('should properly parse blank lines', function() {
+    var blank = tokenRegExps['blank'];
+    var execResult;
+
+    var minimal = "";
+    execResult = blank.exec(minimal);
+    assert(regExpResultEquals(execResult, [""]), "Failed on '', got:\n["+execResult+"]");
+
+    var oneSpace = " ";
+    execResult = blank.exec(oneSpace);
+    assert(regExpResultEquals(execResult, [" "]), "Failed on ' ', got:\n["+execResult+"]");
+
+    var manySpaces = "          ";
+    execResult = blank.exec(manySpaces);
+    assert(regExpResultEquals(execResult, ["          "]), "Failed on '          ', got:\n["+execResult+"]");
+
+    var oneTab = "\t";
+    execResult = blank.exec(oneTab);
+    assert(regExpResultEquals(execResult, ["\t"]), "Failed on '\\t', got:\n["+execResult+"]");
+
+    var mixture = "   \t   \t  ";
+    execResult = blank.exec(mixture);
+    assert(regExpResultEquals(execResult, ["   \t   \t  "]), "Failed on '   \\t   \\t  ', got:\n["+execResult+"]");
+
+    // Non-matching example
+    var nonSpace = "a";
+    execResult = blank.exec(nonSpace);
+    assert(execResult === null, "Failed on 'a', got:\n["+execResult+"]");
+  });
+
+  it('should properly parse horizontal rules', function() {
+    var hRule = tokenRegExps['horizontalRule'];
+    var execResult;
+
+    var minimal = "-----";
+    execResult = hRule.exec(minimal);
+    assert(regExpResultEquals(execResult, [minimal, ""]),
+      "Failed on '"+minimal+"', got:\n["+execResult+"]");
+
+    var indented = "  \t  -----";
+    execResult = hRule.exec(indented);
+    assert(regExpResultEquals(execResult, [indented, "  \t  "]),
+      "Failed on '"+indented+"', got:\n["+execResult+"]");
+
+    var manyHyphens = "  \t  ----------------";
+    execResult = hRule.exec(manyHyphens);
+    assert(regExpResultEquals(execResult, [manyHyphens, "  \t  "]),
+      "Failed on '"+manyHyphens+"', got:\n["+execResult+"]");
+
+    var trailingSpace = "  \t  -----   ";
+    execResult = hRule.exec(trailingSpace);
+    assert(regExpResultEquals(execResult, [trailingSpace, "  \t  "]),
+      "Failed on '"+trailingSpace+"', got:\n["+execResult+"]");
+
+    //----- Examples that should not match -----
+    var tooFewHyphens = "  \t  ----";
+    execResult = hRule.exec(tooFewHyphens);
+    assert(execResult === null,
+      "Failed on '"+tooFewHyphens+"', got:\n["+execResult+"]");
+
+    var somethingBeforeHyphens = "  \t  hello -----";
+    execResult = hRule.exec(somethingBeforeHyphens);
+    assert(execResult === null,
+      "Failed on '"+somethingBeforeHyphens+"', got:\n["+execResult+"]");
+  });
+
+  it('should properly parse comments', function() {
+    var comment = tokenRegExps['comment'];
+    var execResult;
+
+    var minimal =  "#";
+    execResult = comment.exec(minimal);
+    assert(regExpResultEquals(execResult, [minimal, "", ""]),
+      "Failed on '"+minimal+"', got:\n["+execResult+"]");
+
+    var indented =  "   #";
+    execResult = comment.exec(indented);
+    assert(regExpResultEquals(execResult, [indented, "   ", ""]),
+      "Failed on '"+indented+"', got:\n["+execResult+"]");
+
+    var emptyComment =  "   #         ";
+    execResult = comment.exec(emptyComment);
+    assert(regExpResultEquals(execResult, [emptyComment, "   ", "         "]),
+      "Failed on '"+emptyComment+"', got:\n["+execResult+"]");
+
+    var fullComment =  "   #Here is a normal comment. Let's add some special chars #%*$^%#)%*$(#&%{}{1243368";
+    execResult = comment.exec(fullComment);
+    assert(regExpResultEquals(execResult, [fullComment, "   ", "Here is a normal comment. Let's add some special chars #%*$^%#)%*$(#&%{}{1243368"]),
+      "Failed on '"+fullComment+"', got:\n["+execResult+"]");
+
+    //----- Examples that should not match -----
+    var directive =  "   #+Here is a normal comment. Let's add some special chars #%*$^%#)%*$(#&%{}{1243368";
+    execResult = comment.exec(directive);
+    assert(execResult === null,
+      "Failed on '"+directive+"', got:\n["+execResult+"]");
+
+    var noHashChar =  "   Here is a normal comment. Let's add some special chars #%*$^%#)%*$(#&%{}{1243368";
+    execResult = comment.exec(noHashChar);
+    assert(execResult === null,
+      "Failed on '"+noHashChar+"', got:\n["+execResult+"]");
+  });
+
+  it('should properly parse paragraph lines', function() {
+    var line = tokenRegExps['line'];
+    var execResult;
+
+    var minimal = "";
+    execResult = line.exec(minimal);
+    assert(regExpResultEquals(execResult, [minimal, "", ""]),
+      "Failed on '"+minimal+"', got:\n["+execResult+"]");
+
+    var onlySpaces = "   ";
+    execResult = line.exec(onlySpaces);
+    assert(regExpResultEquals(execResult, [onlySpaces, "   ", ""]),
+      "Failed on '"+onlySpaces+"', got:\n["+execResult+"]");
+
+    var fullLine = "   This line is indented by 3 spaces. All chars are allowed: #%&)^%@$#@)35514875{::{:'\"";
+    execResult = line.exec(fullLine);
+    assert(regExpResultEquals(execResult, [fullLine, "   ", "This line is indented by 3 spaces. All chars are allowed: #%&)^%@$#@)35514875{::{:'\""]),
+      "Failed on '"+fullLine+"', got:\n["+execResult+"]");
+
+    // There is nothing that doesn't match the line regexp - it is a catchall for everthing else whose sole job
+    // is to capture indentation.
   });
 });
